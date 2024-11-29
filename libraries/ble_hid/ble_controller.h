1f7e352567dcd2ad1a2d0c429e5d195e59281fe4
unic8s <erik.lembke@unic8.com>
2024-11-29 23:26:44 +0100

    Change ble functions to separate c files

---
+++ libraries/ble_hid/ble_controller.h
@@ -0,0 +1,16 @@
+#ifndef _BLE_CONTROLLER_H
+#define _BLE_CONTROLLER_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <esp_system.h>
+
+esp_err_t ble_controller_init();
+
+#ifdef __cplusplus
+} /*extern "C"*/
+#endif
+
+#endif

44fd3e91ffcb83903351216aed277769231a7d01
unic8s <erik.lembke@unic8.com>
2024-12-01 18:34:50 +0100

    Add local version generation

--- utils/write-version.py
+++ utils/write-version.py
@@ -1,7 +1,15 @@
 import sys
+import subprocess
+
 
 versionPath = "src/version.h"
-versionNumber = sys.argv[1]
+
+if len(sys.argv) > 1:
+    versionNumber = sys.argv[1]
+else:
+    result = subprocess.run(['git', 'pull'])
+    result = subprocess.run(['git', 'describe', '--abbrev=0', '--tags'], stdout=subprocess.PIPE)
+    versionNumber = result.stdout.decode('utf-8').replace("\n", "")
   
 with open(versionPath, "w") as file: 
     file.write('#define SW_VER "' + versionNumber + '"') 

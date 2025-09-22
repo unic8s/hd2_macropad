a3ad299789aa9e7d3041f5dcf7123e335db241a9
unic8s <erik.lembke@unic8.com>
2025-09-22 21:56:02 +0200

    Updated screens

--- src/ui/screens/ui_About.c
+++ src/ui/screens/ui_About.c
@@ -23,9 +23,9 @@ void ui_About_screen_init(void)
     lv_obj_set_align(ui_LblVersion, LV_ALIGN_TOP_RIGHT);
     lv_label_set_text(ui_LblVersion, "vX.X.X");
     ui_object_set_themeable_style_property(ui_LblVersion, LV_PART_MAIN | LV_STATE_DEFAULT, LV_STYLE_TEXT_COLOR,
-                                           _ui_theme_color_colorTheme);
+                                           _ui_theme_color_colorActive);
     ui_object_set_themeable_style_property(ui_LblVersion, LV_PART_MAIN | LV_STATE_DEFAULT, LV_STYLE_TEXT_OPA,
-                                           _ui_theme_alpha_colorTheme);
+                                           _ui_theme_alpha_colorActive);
     lv_obj_set_style_text_align(ui_LblVersion, LV_TEXT_ALIGN_RIGHT, LV_PART_MAIN | LV_STATE_DEFAULT);
     lv_obj_set_style_text_font(ui_LblVersion, &lv_font_montserrat_28, LV_PART_MAIN | LV_STATE_DEFAULT);
 
